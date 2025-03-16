3a016f36cd7848eb0be9cb10355573d2878e6ff3
MEDLAO <sofiane.medhouny.laouina@gmail.com>
2025-03-16 13:31:46 +0000

    Add health_check(), read_root() and middleware

--- main.py
+++ main.py
@@ -1,9 +1,50 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 from lingua import LanguageDetectorBuilder
+import os
 
 
 app = FastAPI()
+
+RAPIDAPI_SECRET = os.getenv("RAPIDAPI_SECRET")
+
+
+@app.middleware("http")
+async def enforce_rapidapi_usage(request: Request, call_next):
+    # Bypass authentication for the /welcome endpoint
+    if request.url.path == "/":
+        return await call_next(request)
+
+    rapidapi_proxy_secret = request.headers.get("X-RapidAPI-Proxy-Secret")
+
+    if rapidapi_proxy_secret != RAPIDAPI_SECRET:
+        return JSONResponse(status_code=403, content={"error": "Access restricted to RapidAPI users only."})
+
+    return await call_next(request)
+
+
+@app.get("/health")
+def health_check():
+    return {"status": "healthy"}
+
+
+@app.get("/")
+def read_root():
+    welcome_message = (
+        "Welcome!"
+        "¡Bienvenido!"
+        "欢迎!"
+        "नमस्ते!"
+        "مرحبًا!"
+        "Olá!"
+        "Здравствуйте!"
+        "Bonjour!"
+        "বাংলা!"
+        "こんにちは!"
+    )
+    return {"message": welcome_message}
+
 
 # Build detector for all available languages
 detector = LanguageDetectorBuilder.from_all_languages().build()
